8a1fb7d26f17a26a618e10af803359a6bc798bbb
wujun_glb <wujun_glb@qq.com>
2020-10-04 19:20:08 +0800

    登录接口完善，通过token传递用户信息

--- intergration-api-common/src/main/java/com/db/springcloud/entities/User.java
+++ intergration-api-common/src/main/java/com/db/springcloud/entities/User.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
     private Integer id;
-    private String  uid;
+    private String  uid;//用户id
     private String  username;
     private String  password;
 }
